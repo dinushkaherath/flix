d353d646c4ae9b03cb101ff11f1865630abe2052
dinushkaherath <dinushka.herath@gmail.com>
2021-02-01 22:53:11 -0500

    28-added edit & delete actions/links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,20 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user, notice: "Account successfully updated!"
+        else
+            render :edit
+        end
+    end
+    
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -19,6 +33,13 @@ class UsersController < ApplicationController
           render :new
         end
     end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to movies_url, alert: "Account successfully deleted!"
+    end
+    
 
 private
 
