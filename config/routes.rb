57d16779ca00c447ab60ecf01b5bc243611978b7
dinushkaherath <dinushka.herath@gmail.com>
2021-02-03 17:31:59 -0500

    29-created sessions controller & links

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews
   end
+
+  resource :session, only: [:new, :create, :destroy]
+
   resources :users
+
   get "signup" => "users#new"
+  get "signin" => "sessions#new"
 
 end
