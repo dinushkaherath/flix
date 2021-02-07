05069b6e203638d2b0d911165d25d3af97290be8
dinushkaherath <dinushka.herath@gmail.com>
2021-02-07 17:05:15 -0500

    33a-require sigin! & refactor current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+private
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    helper_method :current_user
+
+    def require_signin
+        unless current_user
+            redirect_to signin_path, alert: "Please sign in first!"
+        end
+    end
 end
