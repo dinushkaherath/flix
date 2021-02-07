6dd91446d3b8487a6a95bcd8fba484c1457b517b
dinushkaherath <dinushka.herath@gmail.com>
2021-02-07 16:25:10 -0500

    31- added links to account & settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,9 +28,10 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-          redirect_to @user, notice: "Thanks for signing up!"
+            session[:user_id] = @user.id
+            redirect_to @user, notice: "Thanks for signing up!"
         else
-          render :new
+            render :new
         end
     end
 
