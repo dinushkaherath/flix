b08be4d66ff3dfb34f7cc5b50934fb932208e573
dinushkaherath <dinushka.herath@gmail.com>
2021-02-07 17:19:21 -0500

    33b-added correct user & intended url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
     before_action :require_signin, except: [:new, :create]
+    before_action :require_correct_user, only: [:edit, :update, :destroy]
 
     def index
         @users = User.all
@@ -19,7 +20,6 @@ class UsersController < ApplicationController
     end
     
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             redirect_to @user, notice: "Account successfully updated!"
         else
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
     
 
     def create
-        @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
             redirect_to @user, notice: "Thanks for signing up!"
@@ -39,7 +38,6 @@ class UsersController < ApplicationController
     end
 
     def destroy
-        @user = User.find(params[:id])
         @user.destroy
         session[:user_id] = nil
         redirect_to movies_url, alert: "Account successfully deleted!"
@@ -47,6 +45,11 @@ class UsersController < ApplicationController
     
 
 private
+
+def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to movies_url unless current_user?(@user)
+end
 
 def user_params
     params.require(:user).
