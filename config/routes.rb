4fa55be8d7e8fd5f441a1b094cab982fd7d9d97c
dinushkaherath <dinushka.herath@gmail.com>
2021-02-08 12:21:06 -0500

    36-create favorites resource & associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   
   root "movies#index"
-
+  
   resources :movies do
     resources :reviews
   end
-
+  
   resource :session, only: [:new, :create, :destroy]
-
+  
   resources :users
+  resources :favorites
 
   get "signup" => "users#new"
   get "signin" => "sessions#new"
